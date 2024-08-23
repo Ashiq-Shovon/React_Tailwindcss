ac3774d56089cc4be0f59ca427b2c3ae7557f728
Ashiqur Rahman <ashiqurrahman1748@gmail.com>
2024-08-23 18:11:05 +0600

    only function epmty file is added

--- 1.react-concepts-with-tailwindcss/src/components/mainContent/javascript-concept/javascript-content/Function.js
+++ 1.react-concepts-with-tailwindcss/src/components/mainContent/javascript-concept/javascript-content/Function.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Function = () => {
   return (
-    <div>
-      <h2>this is javascrpt function </h2>
-    </div>
-  )
-}
+    <>
+      <div>
+        <div className="flex flex-1 space-x-6 bg-zinc-500 ">
+      
+            
+          
+        </div>
+        <div className="basis-11/12 bg-orange-300">
+          <h2 className="text-center">content</h2>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Function
+export default Function;
