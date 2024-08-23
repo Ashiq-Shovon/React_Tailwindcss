6ee570a9a1e6602aea72254a2e789cc735698c62
Ashiqur Rahman <ashiqurrahman1748@gmail.com>
2024-08-23 17:49:42 +0600

    in navbar shifted end route from dashbaord to array so that the sate menu of dashboard can stay active all the way

--- 1.react-concepts-with-tailwindcss/src/components/mainContent/chai_aur_javascript/layout/NavBar.js
+++ 1.react-concepts-with-tailwindcss/src/components/mainContent/chai_aur_javascript/layout/NavBar.js
@@ -5,16 +5,46 @@ const NavBar = () => {
     <>
       <div className="bg-slate-500 text-2xl p-10">
         <div className=" ">
-          <NavLink to="." end className={({isActive}) => ( isActive ? "px-5 py-3 rounded-lg bg-red-400" :"px-5 py-3 rounded-lg ")}>
-            Dashboard
-          </NavLink>
-          <NavLink to="string" className={({isActive}) => ( isActive ? " px-5 py-3 rounded-lg bg-red-400" :"px-5 py-3 rounded-lg ")}>
-            string
-          </NavLink>
-          <NavLink to="array" className={({isActive}) => ( isActive ? "px-5 py-3 rounded-lg  bg-red-400" :"px-5 py-3 rounded-lg ")}>
+          <NavLink
+            end
+            to="."
+            className={({ isActive }) =>
+              isActive
+                ? "px-5 py-3 rounded-lg  bg-red-400"
+                : "px-5 py-3 rounded-lg "
+            }
+          >
             Array
           </NavLink>
-          <NavLink to="object" className={({isActive}) => ( isActive ? "px-5 py-3 rounded-lg  bg-red-400" :"px-5 py-3 rounded-lg ")}>
+          <NavLink
+            to="dashboard"
+            className={({ isActive }) =>
+              isActive
+                ? "px-5 py-3 rounded-lg bg-red-400"
+                : "px-5 py-3 rounded-lg "
+            }
+          >
+            Dashboard
+          </NavLink>
+          <NavLink
+            to="string"
+            className={({ isActive }) =>
+              isActive
+                ? " px-5 py-3 rounded-lg bg-red-400"
+                : "px-5 py-3 rounded-lg "
+            }
+          >
+            string
+          </NavLink>
+
+          <NavLink
+            to="object"
+            className={({ isActive }) =>
+              isActive
+                ? "px-5 py-3 rounded-lg  bg-red-400"
+                : "px-5 py-3 rounded-lg "
+            }
+          >
             Object
           </NavLink>
         </div>
